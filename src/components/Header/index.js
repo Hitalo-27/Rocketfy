a579ff8c5a7d9b7084de7d8897a8975afbc4604c
Hitalo-27 <56124084+Hitalo-27@users.noreply.github.com>
2021-07-27 19:57:44 -0300

    primeiro commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from '../Header/styles';
+
+export default function Header() {
+    return (
+
+            <Container > 
+                <h1>Rocketfy</h1>
+            </Container>
+            
+
+    )
+}
---
+++ src/services/api.js
@@ -0,0 +1,97 @@
+export function loadLists() {
+    return [
+      { 
+        title: 'Tarefas', 
+        creatable: true,
+        cards: [
+          {
+            id: 1,
+            content: 'Estudar módulo 01 de NodeJS',
+            labels: ['#7159c1'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 2,
+            content: 'Criar vídeo para o Youtube ensinando a recriar a interface do Pipefy',
+            labels: ['#7159c1'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 3,
+            content: 'Estudar módulo 03 de React Native',
+            labels: ['#7159c1'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 4,
+            content: 'Gravar Aula "NextJS: Utilizando server-side rendering com ReactJS"',
+            labels: ['#54e1f7'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 5,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+        ]
+      },
+      { 
+        title: 'Fazendo', 
+        creatable: false,
+        cards: [
+          {
+            id: 6,
+            content: 'Recriando clone do Pipefy',
+            labels: [],
+            user: 'https://github.com/Hitalo-27.png'
+          }
+        ]
+      },
+      { 
+        title: 'Pausado', 
+        creatable: false,
+        cards: [
+          {
+            id: 7,
+            content: 'Gravar sobre Geolocalização e mapas com React Native',
+            labels: ['#7159c1'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 8,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+            user: 'https://github.com/Hitalo-27.png'
+          },
+          {
+            id: 9,
+            content: 'Ajustes na biblioteca unform',
+            labels: [],
+          }
+        ]
+      },
+      { 
+        title: 'Concluído', 
+        creatable: false,
+        done: true,
+        cards: [
+          {
+            id: 10,
+            content: 'Gravar aula sobre deploy e CI com React Native',
+            labels: [],
+          },
+          {
+            id: 12,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+          },
+          {
+            id: 13,
+            content: 'Gravar Aula "Internacionalização de aplicações Node.js, ReactJS e React Native"',
+            labels: ['#7159c1'],
+          }
+        ]
+      },
+    ];
+  }
